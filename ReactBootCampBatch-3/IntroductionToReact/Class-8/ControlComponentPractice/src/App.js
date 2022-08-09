b3de9a6da7aa86b5f3a0297f2c75b809dec7145b
Harunur5252 <hroshid695@gmil.com>
2022-08-09 11:07:03 +0600

    react control and unidirectional way form handling project finally done

--- ReactBootCampBatch-3/IntroductionToReact/Class-8/ControlComponentPractice/src/App.js
+++ ReactBootCampBatch-3/IntroductionToReact/Class-8/ControlComponentPractice/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import FormDataValidation from './FormDataValidation';
@@ -42,11 +42,138 @@ function App() {
       (88)01759995363
       
   */
+
+   // initial user data for tracking
+   const [userData,setUserData] = useState({
+         fullName:'',
+         userName:'',
+         mobileNumber:'',
+         validUrl:'',
+         email:'',
+         password:'',
+         confirmPassword:'',
+   })
+
+   // initial user errors data for tracking
+   const [errors,setErrors] = useState({
+      fullName:'',
+      userName:'',
+      mobileNumber:'',
+      validUrl:'',
+      email:'',
+      password:'',
+      confirmPassword:'',
+   })
+   
+   // for avoiding react batch update, we have set some initial user errors data
+   const userDataErrors = {
+      fullName:'',
+      userName:'',
+      mobileNumber:'',
+      validUrl:'',
+      email:'',
+      password:'',
+      confirmPassword:'',
+   }
+
+  // for tracking loading animation, data submitted,text change 
+  const [loading,setLoading] = useState(false)
+  const [submitted,setSubmitted] = useState(false)
+  const [text,setText] = useState(true)
+
+  // user can change data in form
+  const handleChange = (evt) => {
+   setUserData({
+       ...userData,
+       [evt.target.name] : evt.target.value
+   })
+   setErrors({
+       ...errors,
+       [evt.target.name]:''
+   })
+ }
+ const {fullName,userName,mobileNumber,validUrl,email,password,confirmPassword} = userData
+
+ // If user give errors data format then it's check
+ const formErrorHandle = () => {
+
+   const fullNameRegex = /^[a-z_-]{5,15}$/gi
+   if(fullName === ''){
+       userDataErrors.fullName = 'fullName is required'
+   }
+   if(!fullNameRegex.test(fullName)){
+       userDataErrors.fullName = 'fullName is invalid'
+   }
+   const userNameRegex = /^[\w-.]{4,15}$/gi
+   if(userName === ''){
+       userDataErrors.userName = 'userName is required'
+   }
+   if(!userNameRegex.test(userName)){
+       userDataErrors.userName = 'userName is invalid'
+   }
+   const validMobileRegex = /^\+?(\(?88\)?)?\d{11}$/g
+   if(mobileNumber === ''){
+       userDataErrors.mobileNumber = 'mobile number is required'
+   } 
+   if(!validMobileRegex.test(mobileNumber.trim())){
+       userDataErrors.mobileNumber = 'mobile number is invalid'
+   }
+   const validUrlRegex = /(http(s)?)?[\S]?(:)?[\S]?(\/\/)?[\S]?(www)?[\S]?(\.)?[\S]?[\w-_]{2,20}[\S]?\.[\S]?[a-z]{2,10}(\.bd)?/gi
+   if(validUrl === ''){
+       userDataErrors.validUrl = 'valid url is required'
+   }
+    if(!validUrlRegex.test(validUrl.trim())){
+       userDataErrors.validUrl = 'url is invalid'
+   }
+   const validEmailRegex = /[a-z]{2,5}[\S]?([\d]{2,4})?(\.?-?_?)([a-z]{2,4})?@[\S]?[a-z]{2,5}([\d]{2,4})?(_?)([\S]?)\.[\S]?[a-z]{2,3}/gi
+   
+   if(email === ''){
+       userDataErrors.email = 'email is required'
+   }
+    if(!validEmailRegex.test(email.trim())){
+       userDataErrors.email = 'email must be valid'
+   }
+   const passwordRegex = /^[\w@#\$%\^&\*]{10}$/gi
+   if(password === ''){
+       userDataErrors.password = 'password is required'
+   }
+   if(!passwordRegex.test(password)){
+       userDataErrors.password = 'password is invalid'
+   }
+   if(confirmPassword === ''){
+       userDataErrors.confirmPassword = 'confirm password is required'
+   }
+   if(confirmPassword !== password){
+       userDataErrors.confirmPassword = 'confirm password is not matched'
+   }
+   setErrors(userDataErrors)
+ }
+
+ // data successfully submitted
+ const handleSubmit = (evt) => {
+   evt.preventDefault()
+
+   // from validation for valid format data
+   formErrorHandle()
+
+   // if any error occur then it's called
+   if(Object.values(userDataErrors).some(elm => elm.length > 0)){
+       return 
+   }
+
+   // when data is ready to  submit then it's take 1.2s to submit.  
+   setTimeout(() => {
+       setSubmitted(true)
+       setText(false)
+   },1200)
+   setLoading(true)
+ }
+
   return(
     <>
        <div>
-          <h3 className='form-heading'>Form Validation</h3>
-          <FormDataValidation />
+          <h3 className='form-heading'>{text && 'Form Validation'}</h3>
+          <FormDataValidation  handleChange={handleChange} userData={userData} errors={errors} handleSubmit={handleSubmit} loading={loading} submitted={submitted}  />
        </div>
     </>
   )
--- ReactBootCampBatch-3/IntroductionToReact/Class-8/ControlComponentPractice/src/FormDataValidation.js
+++ ReactBootCampBatch-3/IntroductionToReact/Class-8/ControlComponentPractice/src/FormDataValidation.js
@@ -1,171 +1,67 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Form,Button } from 'react-bootstrap';
 
-export default function FormDataValidation() {
-    const [userData,setUserData] = useState({
-        fullName:'',
-        userName:'',
-        mobileNumber:'',
-        validUrl:'',
-        email:'',
-        password:'',
-        confirmPassword:'',
-    })
+export default function FormDataValidation({handleChange,userData,errors,handleSubmit,loading,submitted}) {
 
-    const [errors,setErrors] = useState({
-        fullName:'',
-        userName:'',
-        mobileNumber:'',
-        validUrl:'',
-        email:'',
-        password:'',
-        confirmPassword:'',
-    })
-
-    const [submitted,setSubmitted] = useState(false)
-
-    const {fullName,userName,mobileNumber,validUrl,email,password,confirmPassword} = userData
-
-  const handleChange = (evt) => {
-    setUserData({
-        ...userData,
-        [evt.target.name] : evt.target.value
-    })
-    setErrors({
-        ...errors,
-        [evt.target.name]:''
-    })
-  }
-
-  const userDataErrors = {
-    fullName:'',
-    userName:'',
-    mobileNumber:'',
-    validUrl:'',
-    email:'',
-    password:'',
-    confirmPassword:'',
-}
-
-  const formErrorHandle = () => {
-
-    const fullNameRegex = /^[a-z_-]{5,15}$/gi
-    if(fullName === ''){
-        userDataErrors.fullName = 'fullName is required'
-    }
-    if(!fullNameRegex.test(fullName)){
-        userDataErrors.fullName = 'fullName is invalid'
-    }
-    const userNameRegex = /^[\w-.]{4,15}$/gi
-    if(userName === ''){
-        userDataErrors.userName = 'userName is required'
-    }
-    if(!userNameRegex.test(userName)){
-        userDataErrors.userName = 'userName is invalid'
-    }
-    const validMobileRegex = /^\+?(\(?88\)?)?\d{11}$/g
-    if(mobileNumber === ''){
-        userDataErrors.mobileNumber = 'mobile number is required'
-    } 
-    if(!validMobileRegex.test(mobileNumber.trim())){
-        userDataErrors.mobileNumber = 'mobile number is invalid'
-    }
-    const validUrlRegex = /(http(s)?)?[\S]?(:)?[\S]?(\/\/)?[\S]?(www)?[\S]?(\.)?[\S]?[\w-_]{2,20}[\S]?\.[\S]?[a-z]{2,10}(\.bd)?/gi
-    if(validUrl === ''){
-        userDataErrors.validUrl = 'valid url is required'
-    }
-     if(!validUrlRegex.test(validUrl.trim())){
-        userDataErrors.validUrl = 'url is invalid'
-    }
-    const validEmailRegex = /[a-z]{2,5}[\S]?([\d]{2,4})?(\.?-?_?)([a-z]{2,4})?@[\S]?[a-z]{2,5}([\d]{2,4})?(_?)([\S]?)\.[\S]?[a-z]{2,3}/gi
-    
-    if(email === ''){
-        userDataErrors.email = 'email is required'
-    }
-     if(!validEmailRegex.test(email.trim())){
-        userDataErrors.email = 'email must be valid'
-    }
-    const passwordRegex = /^[\w@#\$%\^&\*]{10}$/gi
-    if(password === ''){
-        userDataErrors.password = 'password is required'
-    }
-    if(!passwordRegex.test(password)){
-        userDataErrors.password = 'password is invalid'
-    }
-    if(confirmPassword === ''){
-        userDataErrors.confirmPassword = 'confirm password is required'
-    }
-    if(confirmPassword !== password){
-        userDataErrors.confirmPassword = 'confirm password is not matched'
-    }
-    setErrors(userDataErrors)
-  }
-
-  const handleSubmit = (evt) => {
-    evt.preventDefault()
-    formErrorHandle()
-    if(Object.values(userDataErrors).some(elm => elm.length > 0)){
-        return 
-    }
-    setSubmitted(true)
-  }
-
+  const {fullName,userName,mobileNumber,validUrl,email,password,confirmPassword} = userData
 
   return (
     <>
         {
-            submitted ? <h3 className='form-heading'>Successfully submitted</h3>  : <Form className='formInput' onSubmit={handleSubmit} noValidate>
-            <Form.Group className="mb-3" controlId="formBasicFullName">
-                <Form.Label>FullName</Form.Label>
-                <Form.Control type="text" name='fullName' onChange={handleChange} value={fullName} placeholder="FullName" />
-                <span style={{color:'red'}}>{errors.fullName}</span>
-            </Form.Group>
+            submitted ? <h3 className='form-submit'>Successfully submitted</h3>  
+            : 
+            <Form className='formInput' onSubmit={handleSubmit} noValidate>
+                <Form.Group className="mb-3" controlId="formBasicFullName">
+                    <Form.Label>FullName</Form.Label>
+                    <Form.Control type="text" name='fullName' className={errors.fullName ? 'error-class' : 'success-class'} onChange={handleChange} value={fullName} placeholder="FullName" />
+                    <span style={{color:'red'}}>{errors.fullName}</span>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicUserName">
-                <Form.Label>UserName</Form.Label>
-                <Form.Control type="text"  name='userName' onChange={handleChange} value={userName} placeholder="UserName" />
-                <span style={{color:'red'}}>{errors.userName}</span>
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicUserName">
+                    <Form.Label>UserName</Form.Label>
+                    <Form.Control type="text"  name='userName' className={errors.userName ? 'error-class' : 'success-class'} onChange={handleChange} value={userName} placeholder="UserName" />
+                    <span style={{color:'red'}}>{errors.userName}</span>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicMobileNumber">
-                <Form.Label>MobileNumber</Form.Label>
-                <Form.Control type="text" name='mobileNumber' onChange={handleChange} value={mobileNumber} placeholder="MobileNumber" />
-                <span style={{color:'red'}}>{errors.mobileNumber}</span>
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicMobileNumber">
+                    <Form.Label>MobileNumber</Form.Label>
+                    <Form.Control type="text" name='mobileNumber' className={errors.mobileNumber ? 'error-class' : 'success-class'} onChange={handleChange} value={mobileNumber} placeholder="MobileNumber" />
+                    <span style={{color:'red'}}>{errors.mobileNumber}</span>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicValidUrl">
-                <Form.Label>ValidUrl</Form.Label>
-                <Form.Control type="text" name='validUrl' onChange={handleChange} value={validUrl} placeholder="ValidUrl" />
-                <span style={{color:'red'}}>{errors.validUrl}</span>
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicValidUrl">
+                    <Form.Label>ValidUrl</Form.Label>
+                    <Form.Control type="text" name='validUrl' className={errors.validUrl ? 'error-class' : 'success-class'} onChange={handleChange} value={validUrl} placeholder="ValidUrl" />
+                    <span style={{color:'red'}}>{errors.validUrl}</span>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" name='email' onChange={handleChange} value={email} placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                     We'll never share your email with anyone else.
-                </Form.Text><br />
-                <span style={{color:'red'}}>{errors.email}</span>
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>EmailAddress</Form.Label>
+                    <Form.Control type="email" name='email' className={errors.email ? 'error-class' : 'success-class'} onChange={handleChange} value={email} placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text><br />
+                    <span style={{color:'red'}}>{errors.email}</span>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" name='password' onChange={handleChange} value={password} placeholder="Password" />
-                <span style={{color:'red'}}>{errors.password}</span>
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' className={errors.password ? 'error-class' : 'success-class'} onChange={handleChange} value={password} placeholder="Password" />
+                    <span style={{color:'red'}}>{errors.password}</span>
+                </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
-                <Form.Label>ConfirmPassword</Form.Label>
-                <Form.Control type="password" name='confirmPassword' onChange={handleChange} value={confirmPassword} placeholder="ConfirmPassword" />
-                <span style={{color:'red'}}>{errors.confirmPassword}</span>
-            </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+                    <Form.Label>ConfirmPassword</Form.Label>
+                    <Form.Control type="password" name='confirmPassword' className={errors.confirmPassword ? 'error-class' : 'success-class'} onChange={handleChange} value={confirmPassword} placeholder="ConfirmPassword" />
+                    <span style={{color:'red'}}>{errors.confirmPassword}</span>
+                </Form.Group>
 
-            <div className="d-grid gap-2">
-                <Button className='mb-5 btn btn-success' type="submit">
-                    Submit
-                </Button>
-            </div>
-        </Form>
+                <div className="d-grid gap-2">
+                    <Button className='mb-5 btn btn-success' type="submit">
+                        {loading ? 'Loading....' : 'Submit'}
+                    </Button>
+                </div>
+            </Form>
         }
         
     </>
